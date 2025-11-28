fe266a7d4c4e5d25d614e2f07248c9090e4d6049
Anagha PH <anagha.menon@deeptrace.in>
2025-11-28 11:35:11 +0530

    changes for deplyment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -155,6 +155,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 # Celery / Redis
 CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
 CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/1')
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
 
 
 REST_FRAMEWORK = {
